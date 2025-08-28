8c97d22e29124de06bfdcdc92a5eb76391b00b08
Benjamin <benabolade@gmail.com>
2025-08-28 10:56:13 +0100

    ui: add last update to pricing section

--- src/components/sections/pricing.tsx
+++ src/components/sections/pricing.tsx
@@ -1,7 +1,41 @@
 import React from "react";
-import { IconTag } from "@tabler/icons-react";
+import { IconCheck, IconTag } from "@tabler/icons-react";
 import { SectionHeadingProps } from "@/types";
+import { Icons } from "../icons";
 import { SectionContainer, SectionContent, SectionHeader, SectionHeading } from "../section";
+import Button from "../ui/button";
+import Card from "../ui/card";
+
+const pricingPlans = [
+	{
+		name: "Starter",
+		price: "$50",
+		features: ["3 Automated Workflows", "Basic AI Assistant Access", "Email + Slack Integration", "Monthly Performance Reports", "Email Support"],
+	},
+	{
+		name: "Pro",
+		price: "$90",
+		isPopular: true,
+		features: [
+			"10+ Automated Workflows",
+			"Advanced AI Assistant Features",
+			"Bi-Weekly Strategy Reviews",
+			"CRM + Marketing Tool Integrations",
+			"Priority Support",
+		],
+	},
+	{
+		name: "Enterprise",
+		price: "Custom",
+		features: [
+			"Unlimited Custom Workflows",
+			"Dedicated AI Strategist",
+			"API & Private Integrations",
+			"Real-Time Performance Dashboards",
+			"24/7 Premium Support + SLA",
+		],
+	},
+];
 
 const headingProps: SectionHeadingProps = {
 	text: "Flexible Plans for",
@@ -18,13 +52,53 @@ export default function Pricing() {
 				<SectionHeading {...headingProps} />
 			</SectionHeader>
 
-			<SectionContent>
+			<SectionContent className="flex-col gap-8">
 				<div>
-					<div className="inset-shadow-1 w-full max-w-85 mx-auto rounded-lg bg-dark-gray p-1 text-sm">
-						<button className="h-9.5 px-2 py-1.5 bg-black-base">Monthly</button>
-						<button>
-							Yearly <span>Save 20%</span>
+					<div className="inset-shadow-1 mx-auto flex w-full max-w-85 items-center rounded-lg bg-dark-gray p-1 text-sm *:text-sm/5.5">
+						<button className="h-9.5 flex-1 rounded-md bg-black-base px-2 py-1.5 font-medium">Monthly</button>
+						<button className="inline-flex h-9.5 flex-[1.5] items-center justify-center gap-2 rounded-md font-medium">
+							Yearly <span className="rounded-full bg-black-base px-2.5 py-0.5">Save 20%</span>
 						</button>
+					</div>
+				</div>
+
+				<div className="flex flex-wrap items-center justify-center gap-7.5">
+					{pricingPlans.map((plan, idx) => (
+						<Card key={plan.name} container className="max-w-110 min-w-70 gap-6 px-6 py-8">
+							<h3 className="inline-flex items-center gap-2 font-semibold text-white">
+								<span>{plan.name}</span>
+								{plan.name === "Pro" && (
+									<span className="inline-flex items-center gap-2.5 rounded-[22px] bg-black-base px-2 pt-1.25 pb-1.5 text-sm font-medium inset-shadow-[0px_1px_1px_0px] inset-shadow-[#cfe7ff33]">
+										<Icons.Fire size="20" /> <span>Popular</span>
+									</span>
+								)}
+							</h3>
+							<p className="space-y-1.5">
+								<span className="text-[calc(var(--text-5xl)-4px)] font-semibold">{plan.price}</span>{" "}
+								<span className="text-sm text-light-blue opacity-80">{idx < 2 && "/month"}</span>
+							</p>
+
+							<Button className="text-center" shadow={idx === 1}>
+								Get Started
+							</Button>
+
+							<div className="w-full border-t-[3px] border-dotted border-light-blue-transparent" />
+
+							<ul className="space-y-4">
+								{plan.features.map((feature, idx) => (
+									<li key={idx} className="inline-flex items-center gap-2">
+										<IconCheck size="16" opacity="0.5" /> <p className="opacity-80">{feature}</p>
+									</li>
+								))}
+							</ul>
+						</Card>
+					))}
+				</div>
+
+				<div className="flex items-center justify-center">
+					<div className="mx-auto flex max-w-[500px] gap-4 rounded-lg bg-dark-gray px-3 py-1.5">
+						<Icons.PalmArrowDown />
+						<p className="pe-1.5">We donate 2% of your membership to pediatric wellbeing</p>
 					</div>
 				</div>
 			</SectionContent>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 import { ParentProps } from "@/types";
 import { cx } from "@/utils/cx";
 
-export default function Button({ className, children, ...props }: ParentProps) {
+type ButtonProps = ParentProps & {shadow?: boolean};
+
+export default function Button({ className, children, shadow = true, ...props }: ButtonProps) {
 	return (
 		<div className={cx("relative", className)} {...props}>
 			<Link href="#" className="group relative flex rounded-lg bg-white/5 px-7 py-3.5 font-semibold">
-				<div className="bg-[radial-gradient(25%_50%_at_50%_100%,#ffffff_0%,#ffffff00_100%)] absolute inset-0" />
-				<div className="bg-[radial-gradient(20.7%_50%_at_50%_100%,#ffffff_0%,#ffffff00_100%)] absolute inset-0 blur-lg z-0" />
+				<div className="absolute inset-0 bg-[radial-gradient(25%_50%_at_50%_100%,#ffffff_0%,#ffffff00_100%)]" />
+				{shadow && <div className="absolute inset-0 z-0 bg-[radial-gradient(20.7%_50%_at_50%_100%,#ffffff_0%,#ffffff00_100%)] blur-lg" />}
 				<div className="absolute inset-[2px] rounded-lg bg-black-base" />
-				<div className="relative flex items-center gap-1.5">
+				<div className="relative flex w-full items-center justify-center gap-1.5">
 					{children}
 					<span
 						className="h-[23px] w-[22px] transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5"
